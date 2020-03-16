e2045a52720e2c24b1e9028ffd9a994b69fe69d0
eliyahu <eliyahu299@gmail.com>
2020-03-16 14:51:23 +0200

    added a Autofit to the excel and improved the project appearance

--- CleaningProject.py
+++ CleaningProject.py
@@ -4,12 +4,14 @@ from datetime import timedelta
 import xlsxwriter
 from xlsxwriter.worksheet import Worksheet
 import json
+import win32com.client as win32
+import os
 
 max_range=71
-
+excelName="cleaningProject.xlsx"
 with open('soldier_file.json',encoding="utf-8") as jasonFolder:
     data=json.load(jasonFolder)
-workbook = xlsxwriter.Workbook('עבודת נקיון.xlsx')
+workbook = xlsxwriter.Workbook(excelName)
 worksheet: Union[Worksheet, Any] = workbook.add_worksheet()
 #####################################################################
 date_format = workbook.add_format({'num_format': 'dd/mm/yyyy', 'align': 'left'})
@@ -19,9 +21,11 @@ date_object = datetime.strptime(date_string, "%d/%m/%Y")
 ################################################################
 #lists
 headLines= ["הערות","מפקד", "תורן", "יום","תאריך"]
-days=["ראשון","שני","שלישי","רביעי","חמישי","שישי","שבת"]
+days=data["days"]
 soldiers=data["soldiers"]
 Commanders=data["Commanders"]
+commanderInLine=data['commanderInLine']
+soldierInLine=data["soldierInLine"]
 
 
 #getting the list location
@@ -47,7 +51,7 @@ def AddingHeadLines(col = 0,row=0):
 def settingBorder(col):
         color = workbook.add_format()
         color.set_pattern(1)
-        color.set_bg_color('brown')
+        color.set_bg_color('white')
         worksheet.write(col, len(headLines),"" ,color)
         worksheet.write(col, len(headLines)+1, "", color)
 
@@ -73,10 +77,11 @@ def setCommender(comm):
     worksheet.write(col+ofSet-3,gettingListLocation(headLines,"מפקד")+len(headLines)+2,Commanders[comm])
     worksheet.write(col+ofSet-3,gettingListLocation(headLines,"מפקד"),Commanders[comm2])
 
+
+
+
 AddingHeadLines()
-ofSet=0  #this veriable is used to help sortthe columns (every 7 days there is an ofset)
-commenderInLine=data['commander']
-soldierInLine=data["soldeir"]
+ofSet=0  #this veriable is used to help sort the columns (every 7 days there is an ofset)
 for col in range(1,max_range,1):
     setDate(col)
     settingBorder(col+ofSet)
@@ -88,13 +93,13 @@ for col in range(1,max_range,1):
         setSoldeir(col,sold,sold2)
         soldierInLine+=1
     if(day==6):
-        setCommender(commenderInLine)
-        commenderInLine+=1
+        setCommender(commanderInLine)
+        commanderInLine+=1
         if((col/7)!=((max_range-1)/7)):
              ofSet+=1
              AddingHeadLines(col+ofSet)
         else:
-            data['commander']=((commenderInLine+int(max_range/7))%3)
+            data['commander']=((commanderInLine + int(max_range / 7)) % 3)
             data['soldeir']=(soldierInLine+max_range-int(max_range/7*2)-1)%len(soldiers)
             dateString=date_object.date()+ timedelta(days=col-1+50)
             data["date"]=dateString.strftime('%d/%m/%Y')
@@ -106,8 +111,15 @@ workbook.close()
 
 
 
-
-
+def adjustThecells():
+    path =os.path.abspath(excelName) #for some reason (you can look it up on google excel.Workbooks.Open  function needs full path
+    excel = win32.gencache.EnsureDispatch('Excel.Application')
+    wb = excel.Workbooks.Open(path)
+    ws = wb.Worksheets("Sheet1")
+    ws.Columns.AutoFit()
+    wb.Save()
+    excel.Application.Quit()
+adjustThecells()
 
 
 
